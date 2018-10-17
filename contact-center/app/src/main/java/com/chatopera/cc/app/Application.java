1985dd3f7fe0b4f635a82daa135ae4369c90709b
Hai Liang Wang <hailiang.hl.wang@gmail.com>
2018-10-17 15:43:32 +0800

    Closed #99 使用环境变量配置模块

--- contact-center/app/src/main/java/com/chatopera/cc/app/Application.java
+++ contact-center/app/src/main/java/com/chatopera/cc/app/Application.java
@@ -17,8 +17,10 @@
 package com.chatopera.cc.app;
 
 import com.chatopera.cc.app.basic.MainContext;
-import com.chatopera.cc.util.mobile.MobileNumberUtils;
 import com.chatopera.cc.app.config.StartedEventListener;
+import com.chatopera.cc.util.SystemEnvHelper;
+import com.chatopera.cc.util.mobile.MobileNumberUtils;
+import org.apache.commons.lang.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
@@ -42,8 +44,6 @@ import java.io.IOException;
 @EnableElasticsearchRepositories("com.chatopera.cc.app.persistence.es")
 @EnableAsync
 public class Application {
-    private static final Logger logger = LoggerFactory.getLogger(Application.class);
-
 
     @Value("${web.upload-path}")
     private String uploaddir;
@@ -51,57 +51,75 @@ public class Application {
     @Value("${spring.servlet.multipart.max-file-size}")
     private String multipartMaxUpload;
 
-
     @Value("${spring.servlet.multipart.max-request-size}")
     private String multipartMaxRequest;
 
-	static{
-    	MainContext.model.put("contacts", true) ;
-    	MainContext.model.put("sales", true);
-    	MainContext.model.put("chatbot", true);
+    /**
+     * 记载模块
+     */
+    // 外呼模块
+    private final static boolean isCalloutModule = SystemEnvHelper.parseModuleFlag("CSKEFU_MODULE_CALLOUT");
+    // CRM模块
+    private final static boolean isContactsModule = SystemEnvHelper.parseModuleFlag("CSKEFU_MODULE_CONTACTS");
+    // 聊天机器人模块
+    private final static boolean isChatbotModule = SystemEnvHelper.parseModuleFlag("CSKEFU_MODULE_CHATBOT");
+
+    static {
+        // 外呼模块
+        if (isCalloutModule) {
+            MainContext.model.put("sales", true);
+        }
+        // CRM模块
+        if (isContactsModule) {
+            MainContext.model.put("contacts", true);
+        }
+        // 聊天机器人模块
+        if (isChatbotModule) {
+            MainContext.model.put("chatbot", true);
+        }
     }
 
     /**
      * Init local resources
      */
-    protected static void init(){
+    protected static void init() {
         try {
-            logger.info("init mobile number utils ...");
+            System.out.println("init mobile number utils ...");
             MobileNumberUtils.init();
         } catch (IOException e) {
-            logger.error("init error ", e);
+            e.printStackTrace();
             System.exit(1);
         }
     }
 
-    @Bean   
-    public MultipartConfigElement multipartConfigElement() {   
-            MultipartConfigFactory factory = new MultipartConfigFactory();  
-            factory.setMaxFileSize(multipartMaxUpload); //KB,MB
-            factory.setMaxRequestSize(multipartMaxRequest);
-            factory.setLocation(uploaddir);
-            return factory.createMultipartConfig();   
-    }   
-      
+    @Bean
+    public MultipartConfigElement multipartConfigElement() {
+        MultipartConfigFactory factory = new MultipartConfigFactory();
+        factory.setMaxFileSize(multipartMaxUpload); //KB,MB
+        factory.setMaxRequestSize(multipartMaxRequest);
+        factory.setLocation(uploaddir);
+        return factory.createMultipartConfig();
+    }
+
     @Bean
     public EmbeddedServletContainerCustomizer containerCustomizer() {
 
         return new EmbeddedServletContainerCustomizer() {
             @Override
             public void customize(ConfigurableEmbeddedServletContainer container) {
-            	ErrorPage error = new ErrorPage("/error.html");
-            	container.addErrorPages(error);
+                ErrorPage error = new ErrorPage("/error.html");
+                container.addErrorPages(error);
             }
         };
     }
-    
-	public static void main(String[] args) {
+
+    public static void main(String[] args) {
         Application.init();
-		SpringApplication app = new SpringApplication(Application.class) ;
-		app.setBannerMode(Banner.Mode.CONSOLE);
+        SpringApplication app = new SpringApplication(Application.class);
+        app.setBannerMode(Banner.Mode.CONSOLE);
         app.setAddCommandLineProperties(false);
-		app.addListeners(new StartedEventListener());
-		MainContext.setApplicationContext(app.run(args));
-	}	
+        app.addListeners(new StartedEventListener());
+        MainContext.setApplicationContext(app.run(args));
+    }
 }
 
