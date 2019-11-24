b48752fd9fa60262702e03d715732aff7642a2df
Hai Liang Wang <hain@chatopera.com>
2019-11-24 17:43:40 +0800

    error tolerance when execute alter_uk_consult_invite

--- contact-center/config/sql/upgrade/001.alter_uk_consult_invite.sql
+++ contact-center/config/sql/upgrade/001.alter_uk_consult_invite.sql
@@ -1,3 +1,32 @@
--- Enable whitelist for online users
 USE `cosinee`;
-ALTER TABLE uk_consult_invite add whitelist_mode tinyint(4) DEFAULT '0' COMMENT '启用白名单';
+-- -----------------
+-- prepare variables
+-- -----------------
+
+SET @dbname = DATABASE ( );
+SET @tablename = "uk_consult_invite";
+SET @columnname = "whitelist_mode";
+
+SET @preparedStatement = (
+	SELECT
+	IF
+		(
+			(
+			SELECT
+				COUNT( * ) 
+			FROM
+				INFORMATION_SCHEMA.COLUMNS 
+			WHERE
+				( table_name = @tablename ) 
+				AND ( table_schema = @dbname ) 
+				AND ( column_name = @columnname ) 
+			) > 0,
+			"SELECT 1",
+			CONCAT( "ALTER TABLE ", @tablename, " ADD ", @columnname, " TINYINT ( 4 ) DEFAULT '0' COMMENT '启用白名单';" ) 
+	) 
+);
+PREPARE alterIfNotExists 
+FROM
+	@preparedStatement;
+EXECUTE alterIfNotExists;
+DEALLOCATE PREPARE alterIfNotExists;
