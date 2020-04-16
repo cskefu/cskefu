8dc7d9fd6b237fd0ebe8d72f0d5380fa56d19e52
dengchao@xgtl <2325690622@qq.com>
2020-04-16 10:18:48 +0800

    Fix SpringBootServletInitializer's package path

--- contact-center/app/src/main/java/com/chatopera/cc/ServletInitializer.java
+++ contact-center/app/src/main/java/com/chatopera/cc/ServletInitializer.java
@@ -17,12 +17,12 @@
 package com.chatopera.cc;
 
 import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
-public class ServletInitializer extends SpringBootServletInitializer{
-	
-	@Override  
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {  
-        return application.sources(Application.class);  
-    }  
+public class ServletInitializer extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Application.class);
+    }
 }
