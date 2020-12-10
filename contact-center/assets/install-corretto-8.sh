03db35ad8129664a6a10ef7f248b28f57f308b26
Hai Liang Wang <hain@chatopera.com>
2020-12-10 18:31:15 +0800

    https://github.com/chatopera/cskefu/issues/384 update JDK Url

--- contact-center/assets/install-corretto-8.sh
+++ contact-center/assets/install-corretto-8.sh
@@ -6,13 +6,18 @@
 # constants
 baseDir=$(cd `dirname "$0"`;pwd)
 JDKNAME=java-1.8.0-amazon-corretto-jdk.deb
+## fast download with internal addr
+## replace JDKURL when builing outside chatopera machines, java-1.8.0-amazon-corretto-jdk_8.212.04-2_amd64.deb can be download from Amazon Official Site.
+## https://docs.aws.amazon.com/corretto/latest/corretto-8-ug/downloads-list.html
+JDKURL=http://192.168.2.217:30080/vendors/java/java-1.8.0-amazon-corretto-jdk_8.212.04-2_amd64.deb
+
 # functions
 
 # main 
 [ -z "${BASH_SOURCE[0]}" -o "${BASH_SOURCE[0]}" = "$0" ] || return
 apt-get update && apt-get install -y --no-install-recommends publicsuffix ca-certificates wget java-common
 cd /tmp
-wget --no-check-certificate -O $JDKNAME https://static-public.chatopera.com/vendors/java/java-1.8.0-amazon-corretto-jdk_8.212.04-2_amd64.deb
+wget --no-check-certificate -O $JDKNAME $JDKURL
 dpkg --install $JDKNAME
 rm -rf $JDKNAME
 java -version
