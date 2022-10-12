dc84d07201d2a61706b3c45b6428633f9e54fea6
duke <565572696>
2022-10-12 22:08:35 +0800

    https://github.com/cskefu/cskefu/issues/751 服务重命名

--- cskefu-conversation/src/main/java/com/cskefu/io/CskefuConversationApplication.java
+++ cskefu-conversation/src/main/java/com/cskefu/io/CskefuConversationApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CskefuSessionApplication {
+public class CskefuConversationApplication {
     public static void main(String[] args) {
         SpringApplication.run(CskefuSessionApplication.class, args);
     }
