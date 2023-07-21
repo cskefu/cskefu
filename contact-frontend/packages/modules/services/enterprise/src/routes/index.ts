c89cff615784b2463892a37a015448fca5fe4eaa
Kaifuny <superbiger.github@gmail.com>
2023-07-21 12:53:18 +0800

    add: routes to menus
    Signed-off-by: Kaifuny <superbiger.github@gmail.com>

--- contact-frontend/packages/modules/services/enterprise/src/routes/index.ts
+++ contact-frontend/packages/modules/services/enterprise/src/routes/index.ts
@@ -14,6 +14,139 @@ const routes: RouteRecordRaw[] = [
         path: 'index',
         name: ROUTE_NAME.ENTERPRISE_INDEX,
         component: () => import('../views/HomeView.vue'),
+        meta: {
+          title: '企业服务',
+          isAuth: true,
+          icon: 'el-icon-setting',
+        },
+      },
+      {
+        path: 'chat',
+        name: ROUTE_NAME.ENTERPRISE_KEFU_INDEX,
+        component: () => import('../views/HomeView.vue'),
+        meta: {
+          title: '客服入口设置',
+          isAuth: true,
+          icon: 'el-icon-setting',
+        },
+      },
+      {
+        path: 'channel',
+        name: ROUTE_NAME.ENTERPRISE_CHANNEL_INDEX,
+        component: () => import('../views/HomeView.vue'),
+        meta: {
+          title: '渠道接入',
+          isAuth: true,
+          icon: 'el-icon-setting',
+        },
+      },
+      {
+        path: 'knowledge',
+        name: ROUTE_NAME.ENTERPRISE_KNOWLEDGE_INDEX,
+        component: () => import('../views/HomeView.vue'),
+        meta: {
+          title: '知识库',
+          isAuth: true,
+          icon: 'el-icon-setting',
+        },
+      },
+      {
+        path: 'quality',
+        name: ROUTE_NAME.ENTERPRISE_QUALITY_INDEX,
+        component: () => import('../views/HomeView.vue'),
+        meta: {
+          title: '质检',
+          isAuth: true,
+          icon: 'el-icon-setting',
+        },
+      },
+      {
+        path: 'ou',
+        name: ROUTE_NAME.ENTERPRISE_ADMIN_INDEX,
+        meta: {
+          title: '组织/成员',
+        },
+        children: [
+          {
+            path: 'users',
+            name: ROUTE_NAME.ENTERPRISE_USERS_INDEX,
+            component: () => import('../views/HomeView.vue'),
+            meta: {
+              title: '账号管理',
+              isAuth: true,
+              icon: 'el-icon-setting',
+            },
+          },
+          {
+            path: 'roles',
+            name: ROUTE_NAME.ENTERPRISE_ROLES_INDEX,
+            component: () => import('../views/HomeView.vue'),
+            meta: {
+              title: '角色管理',
+              isAuth: true,
+              icon: 'el-icon-setting',
+            },
+          },
+          {
+            path: 'organization',
+            name: ROUTE_NAME.ENTERPRISE_ORGANIZATIONS_INDEX,
+            component: () => import('../views/HomeView.vue'),
+            meta: {
+              title: '组织架构',
+              isAuth: true,
+              icon: 'el-icon-setting',
+            },
+          },
+        ],
+      },
+      {
+        path: 'feature',
+        name: ROUTE_NAME.ENTERPRISE_FEATURE_INDEX,
+        meta: {
+          title: '功能设置',
+        },
+        children: [
+          {
+            path: 'index',
+            name: ROUTE_NAME.ENTERPRISE_CHAT_SETTING_INDEX,
+            component: () => import('../views/HomeView.vue'),
+            meta: {
+              title: '对话设置',
+              isAuth: true,
+              icon: 'el-icon-setting',
+            },
+          },
+          {
+            path: 'index',
+            name: ROUTE_NAME.ENTERPRISE_CUSTOMER_SETTING_INDEX,
+            component: () => import('../views/HomeView.vue'),
+            meta: {
+              title: '客户设置',
+              isAuth: true,
+              icon: 'el-icon-setting',
+            },
+          },
+          {
+            path: 'index',
+            name: ROUTE_NAME.ENTERPRISE_WORKORDER_SETTING_INDEX,
+            component: () => import('../views/HomeView.vue'),
+            meta: {
+              title: '工单设置',
+              isAuth: true,
+              icon: 'el-icon-setting',
+            },
+          },
+          {
+            path: 'index',
+            name: ROUTE_NAME.ENTERPRISE_LEAVEMESSAGE_SETTING_INDEX,
+            component: () => import('../views/HomeView.vue'),
+            meta: {
+              title: '留言设置',
+              isAuth: true,
+              icon: 'el-icon-setting',
+            },
+          },
+        ],
       },
     ],
   },
--- contact-frontend/packages/modules/services/seats/src/routes/index.ts
+++ contact-frontend/packages/modules/services/seats/src/routes/index.ts
@@ -12,8 +12,82 @@ const routes: RouteRecordRaw[] = [
     children: [
       {
         path: 'index',
-        name: ROUTE_NAME.SEATS_INDEX,
+        name: ROUTE_NAME.SEATS_DASHBOARD_INDEX,
         component: () => import('../views/HomeView.vue'),
+        meta: {
+          title: '坐席看板',
+          isAuth: true,
+          icon: 'el-icon-setting',
+        },
+      },
+      {
+        path: 'index',
+        name: ROUTE_NAME.SEATS_CHATMANAGE_INDEX,
+        component: () => import('../views/HomeView.vue'),
+        meta: {
+          title: '会话管理',
+          isAuth: true,
+          icon: 'el-icon-setting',
+        },
+      },
+      {
+        path: 'index',
+        name: ROUTE_NAME.SEATS_LEAVEMANAGE_INDEX,
+        component: () => import('../views/HomeView.vue'),
+        meta: {
+          title: '留言管理',
+          isAuth: true,
+          icon: 'el-icon-setting',
+        },
+      },
+      {
+        path: 'index',
+        name: ROUTE_NAME.SEATS_SEATSMANAGE_INDEX,
+        component: () => import('../views/HomeView.vue'),
+        meta: {
+          title: '坐席管理',
+          isAuth: true,
+          icon: 'el-icon-setting',
+        },
+      },
+      {
+        path: 'index',
+        name: ROUTE_NAME.SEATS_ROBOT_INDEX,
+        meta: {
+          title: '机器人管理',
+        },
+        children: [
+          {
+            path: 'index',
+            name: ROUTE_NAME.SEATS_ROBOT_DASHBOARD_INDEX,
+            component: () => import('../views/HomeView.vue'),
+            meta: {
+              title: '数据统计',
+              isAuth: true,
+              icon: 'el-icon-setting',
+            },
+          },
+          {
+            path: 'index',
+            name: ROUTE_NAME.SEATS_ROBOT_MANAGE_INDEX,
+            component: () => import('../views/HomeView.vue'),
+            meta: {
+              title: '机器人管理',
+              isAuth: true,
+              icon: 'el-icon-setting',
+            },
+          },
+          {
+            path: 'index',
+            name: ROUTE_NAME.SEATS_INDEX,
+            component: () => import('../views/HomeView.vue'),
+            meta: {
+              title: '机器人设置',
+              isAuth: true,
+              icon: 'el-icon-setting',
+            },
+          },
+        ],
       },
     ],
   },
