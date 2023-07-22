90803370fc3876cb11a8a3217372dc39b23e0308
Kaifuny <superbiger.github@gmail.com>
2023-07-22 15:24:38 +0800

    fix: add page header
    Signed-off-by: Kaifuny <superbiger.github@gmail.com>

--- contact-frontend/packages/shared/ui/src/layouts/MenusLayout.vue
+++ contact-frontend/packages/shared/ui/src/layouts/MenusLayout.vue
@@ -1,5 +1,16 @@
 <script setup lang="ts">
-import { NLayout, NLayoutContent, NLayoutSider } from 'naive-ui'
+import {
+  NLayout,
+  NLayoutContent,
+  NLayoutSider,
+  NPageHeader,
+  NBreadcrumb,
+  NBreadcrumbItem,
+  NAvatar,
+  NSpace,
+  NButton,
+  NDropdown,
+} from 'naive-ui'
 </script>
 <template>
   <n-layout has-sider class="h-full">
@@ -7,7 +18,34 @@ import { NLayout, NLayoutContent, NLayoutSider } from 'naive-ui'
       <slot></slot>
     </n-layout-sider>
     <n-layout-content content-style="padding: 10px">
-      <router-view></router-view>
+      <n-page-header subtitle="subtitle">
+        <template #header>
+          <n-breadcrumb>
+            <n-breadcrumb-item>Github</n-breadcrumb-item>
+            <n-breadcrumb-item>CSKEFU</n-breadcrumb-item>
+            <n-breadcrumb-item>春松客服</n-breadcrumb-item>
+          </n-breadcrumb>
+        </template>
+        <template #avatar>
+          <n-avatar
+            src="https://avatars.githubusercontent.com/u/16386583?v=4"
+          />
+        </template>
+        <template #title>
+          <a style="text-decoration: none; color: inherit">Title</a>
+        </template>
+        <template #extra>
+          <n-space>
+            <n-button>Button</n-button>
+            <n-dropdown placement="bottom-start">
+              <n-button :bordered="false" style="padding: 0 4px">
+                ···
+              </n-button>
+            </n-dropdown>
+          </n-space>
+        </template>
+        <router-view></router-view>
+      </n-page-header>
     </n-layout-content>
   </n-layout>
 </template>
