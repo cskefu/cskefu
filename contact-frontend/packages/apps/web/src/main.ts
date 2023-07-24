3be7f9ea4d505aacf5bff8156feede9a9fb82035
Kaifuny <superbiger.github@gmail.com>
2023-07-24 10:53:25 +0800

    add: pinia for apperance
    Signed-off-by: Kaifuny <superbiger.github@gmail.com>

--- contact-frontend/packages/apps/web/src/main.ts
+++ contact-frontend/packages/apps/web/src/main.ts
@@ -3,5 +3,6 @@ import App from './App.vue'
 import './index.css'
 import router from './router'
 import i18n from '@cskefu/i18n'
+import { pinia } from '@cskefu/common'
 
-createApp(App).use(router).use(i18n).mount('#app')
+createApp(App).use(router).use(i18n).use(pinia).mount('#app')
