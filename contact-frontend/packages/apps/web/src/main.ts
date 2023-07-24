729adb70c184ce66d4eb33ff85bf9cff488033d6
Kaifuny <superbiger.github@gmail.com>
2023-07-24 12:44:19 +0800

    chore: change project module name
    Signed-off-by: Kaifuny <superbiger.github@gmail.com>

--- contact-frontend/packages/apps/web/src/main.ts
+++ contact-frontend/packages/apps/web/src/main.ts
@@ -3,6 +3,6 @@ import App from './App.vue'
 import './index.css'
 import router from './router'
 import i18n from '@cskefu/i18n'
-import { pinia } from '@cskefu/common'
+import { pinia } from '@cskefu/logic'
 
 createApp(App).use(router).use(i18n).use(pinia).mount('#app')
