12cb53bba4cdab5f686630351e408cc87f39ec0b
Kaifuny <superbiger.github@gmail.com>
2023-07-25 11:04:10 +0800

    fix: collapse sidebar icon
    Signed-off-by: Kaifuny <superbiger.github@gmail.com>

--- contact-frontend/packages/modules/services/system/src/layouts/MenusLayout.vue
+++ contact-frontend/packages/modules/services/system/src/layouts/MenusLayout.vue
@@ -1,16 +1,23 @@
 <script setup lang="ts">
-import { MenusLayout } from '@cskefu/shared-ui'
+import { ref } from 'vue'
 import { MenuOption } from 'naive-ui'
+
 import { Menu } from '@cskefu/shared-ui'
+import { MenusLayout } from '@cskefu/shared-ui'
+import { routesToMenus } from '@cskefu/shared-utils'
 
 import routes from '../routes'
 
-import { routesToMenus } from '@cskefu/shared-utils'
-
 const menuOptions: MenuOption[] = routesToMenus(routes)
+const collapsed = ref<boolean>(false)
 </script>
 <template>
-  <MenusLayout>
-    <Menu :options="menuOptions" />
+  <MenusLayout v-model:collapsed="collapsed">
+    <Menu
+      v-model:collapsed="collapsed"
+      :options="menuOptions"
+      :collapsed-width="64"
+      :collapsed-icon-size="24"
+    />
   </MenusLayout>
 </template>
