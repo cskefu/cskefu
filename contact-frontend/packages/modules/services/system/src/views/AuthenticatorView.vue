81f9bac4e420e3697a615531bbafd9326b251bdd
Kaifuny <superbiger.github@gmail.com>
2023-07-29 22:20:27 +0800

    fix: tabs name
    Signed-off-by: Kaifuny <superbiger.github@gmail.com>

--- contact-frontend/packages/modules/services/system/src/views/AuthenticatorView.vue
+++ contact-frontend/packages/modules/services/system/src/views/AuthenticatorView.vue
@@ -7,7 +7,6 @@ import { NTabs, NTab } from 'naive-ui'
       <n-tab name="users"> 用户 </n-tab>
       <n-tab name="social"> 登录方式 </n-tab>
       <n-tab name="template"> 模板 </n-tab>
-      <n-tab name="dashboard"> 使用情况 </n-tab>
       <n-tab name="setting"> 设置 </n-tab>
     </n-tabs>
   </div>
--- contact-frontend/packages/modules/services/system/src/views/LogView.vue
+++ contact-frontend/packages/modules/services/system/src/views/LogView.vue
@@ -4,10 +4,7 @@ import { NTabs, NTab } from 'naive-ui'
 <template>
   <div class="p-4">
     <n-tabs type="line" :bar-width="32">
-      <n-tab name="users"> 用户 </n-tab>
-      <n-tab name="social"> 登录方式 </n-tab>
-      <n-tab name="template"> 模板 </n-tab>
-      <n-tab name="dashboard"> 使用情况 </n-tab>
+      <n-tab name="users"> 日志 </n-tab>
       <n-tab name="setting"> 设置 </n-tab>
     </n-tabs>
   </div>
