02ee158dcb5b1c6a51b11baae362bcf6b1641d1f
Kaifuny <superbiger.github@gmail.com>
2023-08-17 09:22:33 +0800

    add: log svg
    Signed-off-by: Kaifuny <superbiger.github@gmail.com>

--- contact-frontend/packages/modules/services/chat/src/layouts/MenusLayout.vue
+++ contact-frontend/packages/modules/services/chat/src/layouts/MenusLayout.vue
@@ -1,11 +1,17 @@
 <script setup lang="ts">
 import { MenusLayout } from '@cskefu/shared-ui'
 import { MenuOption, NMenu } from 'naive-ui'
+import { ref } from 'vue'
 
 const menuOptions: MenuOption[] = []
+const collapsed = ref<boolean>(false)
 </script>
 <template>
-  <MenusLayout>
-    <n-menu :options="menuOptions" />
+  <MenusLayout v-model:collapsed="collapsed">
+    <n-menu
+      :options="menuOptions"
+      :collapsed-width="64"
+      :collapsed-icon-size="24"
+    />
   </MenusLayout>
 </template>
