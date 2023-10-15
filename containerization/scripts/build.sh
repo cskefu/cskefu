89aa922622c42a26fcdac4870b91fb0dcafaa976
lecjy <565572696@qq.com>
2023-10-15 20:28:37 +0800

    sentinel完善

--- containerization/scripts/build.sh
+++ containerization/scripts/build.sh
@@ -4,6 +4,14 @@ docker build -f openjdk-21-debian-stretch-slim -t openjdk-21:debian-stretch-slim
 
 docker tag openjdk-21:debian-stretch-slim cskefu/openjdk-21:debian-stretch-slim
 
+
+docker build -f sentinel-dashboard-1.8.6 -t sentinel-dashboard:1.8.6 .
+
+docker tag sentinel-dashboard:1.8.6 cskefu/sentinel-dashboard:1.8.6
+
+docker tag sentinel-dashboard:1.8.6 cskefu/sentinel-dashboard:latest
+
 docker login
 
+docker push cskefu/sentinel-dashboard:1.8.6
 docker push cskefu/openjdk-21:debian-stretch-slim
--- cskefu-backend/cskefu-gateway/cskefu-web-gateway/src/main/java/com/cskefu/CskefuWebGatewayApplication.java
+++ cskefu-backend/cskefu-gateway/cskefu-web-gateway/src/main/java/com/cskefu/CskefuWebGatewayApplication.java
@@ -3,6 +3,7 @@ package com.cskefu;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
